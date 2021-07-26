e554aaf800214a9e05ea964bf4d408650855737c
Mahmoud Atwa <m.atwa@hotmail.com>
2021-07-26 05:18:08 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   MPADiff v1.0.1
+ *   MPADiff v1.0.2
  *   https://github.com/hodgef/ts-library-boilerplate-basic
  *
  *   Copyright (c) Mahmoud Atwa (https://github.com/atwamahmoud) and project contributors.
